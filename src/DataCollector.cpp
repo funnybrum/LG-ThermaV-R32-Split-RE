48e30a64d96067de8a4ccb8752eeded3367ccb42
brum <funnybrum@gmail.com>
2025-11-06 08:39:27 +0200

    A few minor code improvements

--- src/DataCollector.cpp
+++ src/DataCollector.cpp
@@ -23,6 +23,8 @@ void DataCollector::collectData() {
     append("set_temp", thermaV.getHeatingSetTemp());
     append("flow", thermaV.getFlow(), 1);
     append("mode", thermaV.getMode());
+    // Don't reset the freshC601 flag, assume data is flowing steadily. The
+    // fresh flag messes data collection intervals.
     // thermaV.resetFreshC601();
 }
 
